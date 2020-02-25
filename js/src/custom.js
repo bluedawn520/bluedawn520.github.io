429b852f2619431baeeb1ee8580c11005eea6275
bluedawn520 <2515294061@qq.com>
2020-02-26 04:02:09 +0800

    Site updated: 2020-02-26 04:01:59

---
+++ js/src/custom.js
@@ -0,0 +1,3 @@
+// build time:Wed Feb 26 2020 03:45:51 GMT+0800 (中国标准时间)
+function randomColor(){return"rgb("+~~(255*Math.random())+","+~~(255*Math.random())+","+~~(255*Math.random())+")"}
+//rebuild by neat 
