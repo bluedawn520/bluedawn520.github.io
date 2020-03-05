bc999a15718b5a46c9a28bca944a5a6ec48f760d
bluedawn520 <2515294061@qq.com>
2020-03-05 17:20:41 +0800

    Site updated: 2020-03-05 17:20:36

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,17 +1,3 @@
-<!--崩溃欺骗-->
- var OriginTitle = document.title;
- var titleTime;
- document.addEventListener('visibilitychange', function () {
-     if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/img/TEP.ico");
-         document.title = '╭(°A°`)╮ 崩溃了 ~';
-         clearTimeout(titleTime);
-     }
-     else {
-         $('[rel="icon"]').attr('href', "/favicon.ico");
-         document.title = '(ฅ>ω<*ฅ) 欢迎来到 ~' + OriginTitle;
-         titleTime = setTimeout(function () {
-             document.title = OriginTitle;
-         }, 2000);
-     }
- });
+// build time:Thu Mar 05 2020 17:20:22 GMT+0800 (GMT+08:00)
+var OriginTitle=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","/img/TEP.ico");document.title="╭(°A°`)╮ 崩溃了 ~";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","/favicon.ico");document.title="(ฅ>ω<*ฅ) 欢迎来到 ~"+OriginTitle;titleTime=setTimeout(function(){document.title=OriginTitle},2e3)}});
+//rebuild by neat 
