c9579eb94e19446685c5bb130b403836adefc377
bluedawn520 <2515294061@qq.com>
2020-03-05 17:25:22 +0800

    Site updated: 2020-03-05 17:25:17

--- js/cursor/text.js
+++ js/cursor/text.js
@@ -1,3 +1,3 @@
-// build time:Thu Mar 05 2020 17:20:22 GMT+0800 (GMT+08:00)
+// build time:Thu Mar 05 2020 17:25:00 GMT+0800 (GMT+08:00)
 var a_idx=0;jQuery(document).ready(function(a){a("body").click(function(e){var i=new Array("富强","民主","文明","和谐","自由","平等","公正","法治","爱国","敬业","诚信","友善");var n=a("<span/>").text(i[a_idx]);a_idx=(a_idx+1)%i.length;var t=e.pageX,o=e.pageY;n.css({"z-index":99999,top:o-28,left:t-i[a_idx].length*8,position:"absolute",color:"#ff7a45"});a("body").append(n);n.animate({top:o-180,opacity:0},1500,function(){n.remove()});a_idx=(a_idx+1)%i.length})});
 //rebuild by neat 
