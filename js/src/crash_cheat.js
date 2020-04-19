5de0650107ba8683b633e784d8fb28c940377317
bluedawn520 <51207352+bluedawn520@users.noreply.github.com>
2020-04-19 17:18:53 +0800

    Site updated: 2020-04-19 17:18:42

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,3 +1,3 @@
-// build time:Sun Apr 19 2020 17:11:40 GMT+0800 (GMT+08:00)
+// build time:Sun Apr 19 2020 17:18:21 GMT+0800 (GMT+08:00)
 var OriginTitle=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","/img/TEP.ico");document.title="╭(°A°`)╮ 崩溃了 ~";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","/favicon.ico");document.title="(ฅ>ω<*ฅ) 欢迎来到 ~"+OriginTitle;titleTime=setTimeout(function(){document.title=OriginTitle},2e3)}});
 //rebuild by neat 
