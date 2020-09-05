785a14e7e7c87e734a69b9c73c8619143548aa12
bluedawn520 <51207352+bluedawn520@users.noreply.github.com>
2020-09-05 23:34:28 +0800

    Site updated: 2020-09-05 23:34:03

--- js/cursor/text.js
+++ js/cursor/text.js
@@ -1,3 +1,3 @@
-// build time:Sat Sep 05 2020 23:13:07 GMT+0800 (GMT+08:00)
+// build time:Sat Sep 05 2020 23:32:47 GMT+0800 (GMT+08:00)
 var a_idx=0;jQuery(document).ready(function(a){a("body").click(function(e){var i=new Array("富强","民主","文明","和谐","自由","平等","公正","法治","爱国","敬业","诚信","友善");var n=a("<span/>").text(i[a_idx]);a_idx=(a_idx+1)%i.length;var t=e.pageX,o=e.pageY;n.css({"z-index":99999,top:o-28,left:t-i[a_idx].length*8,position:"absolute",color:"#ff7a45"});a("body").append(n);n.animate({top:o-180,opacity:0},1500,function(){n.remove()});a_idx=(a_idx+1)%i.length})});
 //rebuild by neat 
--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,3 +1,3 @@
-// build time:Sat Sep 05 2020 23:13:07 GMT+0800 (GMT+08:00)
+// build time:Sat Sep 05 2020 23:32:47 GMT+0800 (GMT+08:00)
 var OriginTitle=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","/img/TEP.ico");document.title="╭(°A°`)╮ 崩溃了 ~";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","/favicon.ico");document.title="(ฅ>ω<*ฅ) 欢迎来到 ~"+OriginTitle;titleTime=setTimeout(function(){document.title=OriginTitle},2e3)}});
 //rebuild by neat 
